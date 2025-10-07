2d1494a05af0116e009a5f15f2367e84387e3ac2
ivanlgodoy <ivanleiva738@gmail.com>
2025-10-07 15:43:08 -0300

    Delete Ivan-TPI directory

--- Ivan-TPI/src/middlewares/routes_validation.js
+++
@@ -1,7 +0,0 @@
-const routes_validation = (req,res,next) => {
-  if (!req.session.user) {
-    return res.redirect('/');
-  };
-  next();
-};
-module.exports = routes_validation;
--- Ivan-TPI/src/middlewares/views_validation.js
+++
@@ -1,9 +0,0 @@
-const db = require("../database/models/models/");
-async function alumnado_validation(req,res,next) {
-  const alumnado = await db.Persona.findAll({where: {tipo_usuario_id: 3}});
-  if (alumnado.tipo_usuario_id != 3 && !req.session.user) {
-    return res.redirect("/");
-  };
-  next();
-};
-module.exports = alumnado_validation;
